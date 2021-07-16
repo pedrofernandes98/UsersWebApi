73edcc6d019330475b7645e17e38838b63442052
pedrofernandes98 <ppedrohfs@gmail.com>
2021-07-15 23:22:10 -0300

    Configurando a primeira migration

--- src/4_Users.Infra/Context/UsersContext.cs
+++ src/4_Users.Infra/Context/UsersContext.cs
@@ -11,7 +11,14 @@ namespace Users.Infra.Context
         public UsersContext(DbContextOptions<UsersContext> options) : base(options)
         { }
 
+
+
         public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=WEBAPIDB;Data Source=LAPTOP-FJ79JN80\SQLEXPRESS");
+        }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
---
+++ src/4_Users.Infra/Migrations/20210716021951_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Users.Infra.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "USER",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "BIGINT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NAME = table.Column<string>(type: "VARCHAR(150)", maxLength: 150, nullable: false),
+                    EMAIL = table.Column<string>(type: "VARCHAR(180)", maxLength: 180, nullable: false),
+                    PASSWORD = table.Column<string>(type: "VARCHAR(180)", maxLength: 180, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_USER", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "USER");
+        }
+    }
+}
