cb985415a7ef4dcf7cbe17e41553542d97b918b8
pedrofernandes98 <ppedrohfs@gmail.com>
2021-07-22 00:32:42 -0300

    Implementacao  de busca por nome e email

--- src/1_Users.Api/Controllers/UserController.cs
+++ src/1_Users.Api/Controllers/UserController.cs
@@ -150,5 +150,109 @@ namespace Users.Api.Controllers
                 return StatusCode(500, Responses.ApplicationErrorMessage());
             }
         }
+
+        [HttpGet]
+        [Route("/api/v1/users/getbyname/{name}")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            try
+            {
+                var user = await _userService.GetByName(name);
+                var message = user == null ? $"Nenhum usuário encontrado com o nome: {name}." : "Usuário encontrado com sucesso!";
+
+                return Ok(new ResultViewModel
+                {
+                    Message = message,
+                    IsSuccess = true,
+                    Data = user
+                });
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
+
+        [HttpGet]
+        [Route("/api/v1/searchbyname/")]
+        public async Task<IActionResult> SearchByName([FromQuery] string name)
+        {
+            try
+            {
+                var users = await _userService.SearchByName(name);
+                var message = users.Count == 0 ? $"Nenhum usuário encontrado com o nome: {name}." : "Lista de usuários recuperada com sucesso!";
+
+                return Ok(new ResultViewModel
+                {
+                    Message = message,
+                    IsSuccess = true,
+                    Data = users
+                });
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
+
+        [HttpGet]
+        [Route("/api/v1/users/getbyemail/")]
+        public async Task<IActionResult> GetByEmail([FromQuery] string email)
+        {
+            try
+            {
+                var user = await _userService.GetByEmail(email);
+                var message = user == null ? $"Nenhum usuário encontrado com o e-mail: {email}." : "Usuário encontrado com sucesso!";
+
+                return Ok(new ResultViewModel
+                {
+                    Message = message,
+                    IsSuccess = true,
+                    Data = user
+                });
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
+
+        [HttpGet]
+        [Route("/api/v1/searchbyemail/")]
+        public async Task<IActionResult> SearchByEmail([FromQuery] string email)
+        {
+            try
+            {
+                var users = await _userService.SearchByEmail(email);
+                var message = users.Count == 0 ? $"Nenhum usuário encontrado com o e-mail: {email}." : "Lista de usuários recuperada com sucesso!";
+
+                return Ok(new ResultViewModel
+                {
+                    Message = message,
+                    IsSuccess = true,
+                    Data = users
+                });
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
     }
 }
